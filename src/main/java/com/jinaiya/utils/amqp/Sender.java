326cc2a7ae6b87f35903615554e42160275a9da6
surick <jk103@qq.com>
2018-11-20 15:04:48 +0800

    add rabbitmq

---
+++ src/main/java/com/jinaiya/utils/amqp/Sender.java
@@ -0,0 +1,8 @@
+package com.jinaiya.utils.amqp;
+
+/**
+ * @author Jin
+ * @date 2018/11/20
+ */
+public class Sender {
+}
---
+++ src/main/java/com/jinaiya/utils/config/RabbitConfig.java
@@ -0,0 +1,8 @@
+package com.jinaiya.utils.config;
+
+/**
+ * @author Jin
+ * @date 2018/11/20
+ */
+public class RabbitConfig {
+}
