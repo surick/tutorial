ffdc7051e2187da9c018bce90ceab416909b4dc7
surick <jk103@qq.com>
2018-12-05 11:07:04 +0800

    add telegram bot api

---
+++ src/main/java/com/jinaiya/utils/commands/HelpCommand.java
@@ -0,0 +1,55 @@
+package com.jinaiya.utils.commands;
+
+import com.jinaiya.utils.database.DatabaseManager;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.ICommandRegistry;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.logging.BotLogger;
+
+/**
+ * This command helps the user to find the command they need
+ *
+ * @author Timo Schulz (Mit0x2)
+ */
+public class HelpCommand extends BotCommand {
+
+    private static final String LOGTAG = "HELPCOMMAND";
+
+    private final ICommandRegistry commandRegistry;
+
+    public HelpCommand(ICommandRegistry commandRegistry) {
+        super("help", "Get all the commands this bot provides");
+        this.commandRegistry = commandRegistry;
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+        if (!DatabaseManager.getInstance().getUserStateForCommandsBot(user.getId())) {
+            return;
+        }
+
+        StringBuilder helpMessageBuilder = new StringBuilder("<b>Help</b>\n");
+        helpMessageBuilder.append("These are the registered commands for this Bot:\n\n");
+
+        for (IBotCommand botCommand : commandRegistry.getRegisteredCommands()) {
+            helpMessageBuilder.append(botCommand.toString()).append("\n\n");
+        }
+
+        SendMessage helpMessage = new SendMessage();
+        helpMessage.setChatId(chat.getId().toString());
+        helpMessage.enableHtml(true);
+        helpMessage.setText(helpMessageBuilder.toString());
+
+        try {
+            absSender.execute(helpMessage);
+        } catch (TelegramApiException e) {
+            BotLogger.error(LOGTAG, e);
+        }
+    }
+}
