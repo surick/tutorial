9a3f08194ca03ea63d7b2a0ac120d694b86aead8
surick <jk103@qq.com>
2018-12-07 12:48:29 +0800

    add AliyunOSS api

---
+++ src/main/java/com/jinaiya/utils/config/AliyunOSSConfig.java
@@ -0,0 +1,71 @@
+package com.jinaiya.utils.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jin
+ * @date 2018/12/7
+ */
+@Component
+@PropertySource(value = "classpath:alioss.properties")
+@ConfigurationProperties(prefix = "aliyun.oss")
+public class AliyunOSSConfig {
+    private String bucketname;
+    private String endpoint;
+    private String keyid;
+    private String keysecret;
+    private String filehost;
+
+    public String getBucketname() {
+        return bucketname;
+    }
+
+    public void setBucketname(String bucketname) {
+        this.bucketname = bucketname;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getKeyid() {
+        return keyid;
+    }
+
+    public void setKeyid(String keyid) {
+        this.keyid = keyid;
+    }
+
+    public String getKeysecret() {
+        return keysecret;
+    }
+
+    public void setKeysecret(String keysecret) {
+        this.keysecret = keysecret;
+    }
+
+    public String getFilehost() {
+        return filehost;
+    }
+
+    public void setFilehost(String filehost) {
+        this.filehost = filehost;
+    }
+
+    @Override
+    public String toString() {
+        return "AliyunOSSConfig{" +
+                "bucketname='" + bucketname + '\'' +
+                ", endpoint='" + endpoint + '\'' +
+                ", keyid='" + keyid + '\'' +
+                ", keysecret='" + keysecret + '\'' +
+                ", filehost='" + filehost + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/jinaiya/utils/controller/AliyunOSSController.java
@@ -0,0 +1,99 @@
+package com.jinaiya.utils.controller;
+
+import com.jinaiya.utils.utils.AliyunOSSUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * @author Jin
+ * @date 2018/12/7
+ */
+@Controller
+@RequestMapping("/oss")
+public class AliyunOSSController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private AliyunOSSUtil aliyunOSSUtil;
+
+    @RequestMapping("/root")
+    public String index() {
+        return "index";
+    }
+
+    /**
+     * 测试上传文件到阿里云OSS存储
+     *
+     * @return
+     */
+    @RequestMapping("/testUpload")
+    @ResponseBody
+    public String testUpload() {
+        File file = new File("/Users/jin/Download/test.jpg");
+        AliyunOSSUtil aliyunOSSUtil = new AliyunOSSUtil();
+        String url = aliyunOSSUtil.upLoad(file);
+        System.out.println(url);
+        return "success";
+    }
+    /**
+     * 通过文件名下载文件
+     */
+    @RequestMapping("/testDownload")
+    @ResponseBody
+    public String testDownload() {
+        AliyunOSSUtil aliyunOSSUtil = new AliyunOSSUtil();
+        aliyunOSSUtil.downloadFile(
+                "test/2018-12-04/e3f892c27f07462a864a43b8187d4562-rawpixel-600782-unsplash.jpg","E:/Picture/e3f892c27f07462a864a43b8187d4562-rawpixel-600782-unsplash.jpg");
+        return "success";
+    }
+    /**
+     * 列出某个文件夹下的所有文件
+     */
+    @RequestMapping("/testListFile")
+    @ResponseBody
+    public String testListFile() {
+        AliyunOSSUtil aliyunOSSUtil = new AliyunOSSUtil();
+        aliyunOSSUtil.listFile();
+        return "success";
+    }
+
+    /**
+     * 文件上传（供前端调用）
+     */
+    @RequestMapping(value = "/uploadFile")
+    public String uploadPicture(@RequestParam("file") MultipartFile file, Model model) {
+        logger.info("文件上传");
+        String filename = file.getOriginalFilename();
+        System.out.println(filename);
+        try {
+
+            if (file != null) {
+                if (!"".equals(filename.trim())) {
+                    File newFile = new File(filename);
+                    FileOutputStream os = new FileOutputStream(newFile);
+                    os.write(file.getBytes());
+                    os.close();
+                    file.transferTo(newFile);
+                    // 上传到OSS
+                    String uploadUrl = aliyunOSSUtil.upLoad(newFile);
+                    model.addAttribute("url",uploadUrl);
+                }
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "success";
+    }
+}
