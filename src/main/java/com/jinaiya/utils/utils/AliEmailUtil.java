fe4913e65e0d4aa05147bdd0e3a865fc42d48b12
surick <jk103@qq.com>
2018-12-17 16:12:59 +0800

    add ali email api

---
+++ src/main/java/com/jinaiya/utils/utils/AliEmailUtil.java
@@ -0,0 +1,56 @@
+package com.jinaiya.utils.utils;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dm.model.v20151123.SingleSendMailRequest;
+import com.aliyuncs.dm.model.v20151123.SingleSendMailResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+
+/**
+ * @author Jin
+ * @date 2018/12/17
+ */
+public class AliEmailUtil {
+
+    public static final String ACCESS_KEY = "";
+    public static final String SECRET_KEY = "";
+
+    public static void sendEmail() {
+        // 如果是除杭州region外的其它region（如新加坡、澳洲Region），需要将下面的"cn-hangzhou"替换为"ap-southeast-1"、或"ap-southeast-2"。
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", ACCESS_KEY, SECRET_KEY);
+        // 如果是除杭州region外的其它region（如新加坡region）， 需要做如下处理
+        //try {
+        //DefaultProfile.addEndpoint("dm.ap-southeast-1.aliyuncs.com", "ap-southeast-1", "Dm",  "dm.ap-southeast-1.aliyuncs.com");
+        //} catch (ClientException e) {
+        //e.printStackTrace();
+        //}
+        IAcsClient client = new DefaultAcsClient(profile);
+        SingleSendMailRequest request = new SingleSendMailRequest();
+        try {
+            //request.setVersion("2017-06-22");// 如果是除杭州region外的其它region（如新加坡region）,必须指定为2017-06-22
+            request.setAccountName("noreply@mail.jinaiya.com");
+            request.setFromAlias("Surick");
+            request.setAddressType(1);
+            request.setTagName("TEST");
+            request.setReplyToAddress(true);
+            request.setToAddress("jk103@qq.com");
+            //可以给多个收件人发送邮件，收件人之间用逗号分开，批量发信建议使用BatchSendMailRequest方式
+            //request.setToAddress("邮箱1,邮箱2");
+            request.setSubject("邮件主题");
+            request.setHtmlBody("邮件正文");
+            SingleSendMailResponse httpResponse = client.getAcsResponse(request);
+        } catch (ServerException e) {
+            e.printStackTrace();
+        }
+        catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        sendEmail();
+    }
+}
