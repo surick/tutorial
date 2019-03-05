6be533d721912d1eda0978d5e8430f06348a9547
surick <jk103@qq.com>
2019-03-05 15:20:53 +0800

    algorithm practices

---
+++ src/main/java/com/jinaiya/tutorials/algorithm/RotateImage.java
@@ -0,0 +1,54 @@
+package com.jinaiya.tutorials.algorithm;
+
+/**
+ * You are given an n x n 2D matrix representing an image.
+ * <p>
+ * Rotate the image by 90 degrees (clockwise).
+ * <p>
+ * Note:
+ * <p>
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given input matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ *
+ * @author Jin
+ * @date 2019/3/5
+ */
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0 ; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        rotate(new int[][]{});
+    }
+}
---
+++ src/main/java/com/jinaiya/tutorials/algorithm/ValidSudoku.java
@@ -0,0 +1,38 @@
+package com.jinaiya.tutorials.algorithm;
+
+import java.util.HashSet;
+
+/**
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * <p>
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * @author Jin
+ * @date 2019/3/4
+ */
+public class ValidSudoku {
+    public static boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i<9; i++){
+            HashSet<Character> rows = new HashSet<Character>();
+            HashSet<Character> columns = new HashSet<Character>();
+            HashSet<Character> cube = new HashSet<Character>();
+            for (int j = 0; j < 9;j++){
+                if(board[i][j]!='.' && !rows.add(board[i][j]))
+                    return false;
+                if(board[j][i]!='.' && !columns.add(board[j][i]))
+                    return false;
+                int RowIndex = 3*(i/3);
+                int ColIndex = 3*(i%3);
+                if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isValidSudoku(new char[][]{});
+    }
+}
